80777de520a23c112ff29ca815194418659c6627
SayaTsutsumi <saya.tsutsumi@gmail.com>
2014-05-01 19:10:10 +0900

    book周りのdb構築

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  resources :fan_comments
+
+  resources :reviews
+
+  resources :authors
+
+  resources :users
+
   resources :books
 
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140501045047) do
+ActiveRecord::Schema.define(version: 20140501094049) do
+
+  create_table "authors", force: true do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.date     "birth"
+    t.text     "address"
+    t.string   "ctype"
+    t.binary   "photo"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "authors", ["user_id"], name: "index_authors_on_user_id"
+
+  create_table "authors_books", id: false, force: true do |t|
+    t.integer "author_id", null: false
+    t.integer "book_id",   null: false
+  end
 
   create_table "books", force: true do |t|
     t.string   "isbn"
@@ -20,6 +38,46 @@ ActiveRecord::Schema.define(version: 20140501045047) do
     t.string   "publish"
     t.date     "published"
     t.boolean  "cd"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "fan_comments", force: true do |t|
+    t.integer  "author_no"
+    t.string   "name"
+    t.text     "body"
+    t.boolean  "deleted"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "memos", force: true do |t|
+    t.string   "memoable_type"
+    t.integer  "memoable_id"
+    t.string   "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "reviews", force: true do |t|
+    t.integer  "book_id"
+    t.integer  "user_id"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "reviews", ["book_id"], name: "index_reviews_on_book_id"
+  add_index "reviews", ["user_id"], name: "index_reviews_on_user_id"
+
+  create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "password"
+    t.string   "salt"
+    t.string   "email"
+    t.boolean  "dm"
+    t.string   "roles"
+    t.integer  "reviews_count"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
