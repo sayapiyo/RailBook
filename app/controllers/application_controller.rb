1ee66c0e02875d5ee8a1793f96f65fc2797892d3
SayaTsutsumi <saya.tsutsumi@gmail.com>
2014-05-08 17:22:47 +0900

    基底クラスでの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  rescue_from ActiveRecord::RecordNotFound, with: :id_invalid
+
+  private
+  def id_invalid(e)
+    render 'shared/record_not_found', status: 404
+  end
 end
