4874e8f04dcc5d6d81b7ecd530d22f41a3bd1394
SayaTsutsumi <saya.tsutsumi@gmail.com>
2014-05-09 17:58:32 +0900

    controllerのテスト

--- test/controllers/hello_controller_test.rb
+++ test/controllers/hello_controller_test.rb
@@ -4,4 +4,11 @@ class HelloControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "list action" do
+    get :list
+    assert_equal 10, assigns(:books).length, 'found rows is wrong.'
+    assert_response :success, 'list action falid.'
+    assert_template 'hello/list'
+  end
 end
